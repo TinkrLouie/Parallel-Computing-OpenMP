111c5c3353cf24ea1cef70004148dcb3a767bfd6
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-13 01:57:57 +0000

    Update magic_matrix.cpp

--- magic_matrix.cpp
+++ magic_matrix.cpp
@@ -117,7 +117,7 @@ bool isPairwiseDistinct( int** matrix, int N) {
     std::unordered_set<int> elementSet;
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for collapse(2) schedule(dynamic, 10) shared(found)
+    #pragma omp parallel for collapse(2) schedule(dynamic, 100) shared(found)
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             int currentElement = matrix[i][j];
