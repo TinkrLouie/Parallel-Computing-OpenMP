2d3724bf672d485ac5b02f193bc5332bae5d68dc
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-14 23:44:41 +0000

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <omp.h>
 #include <unordered_set>
-#define CHUNK_SIZE 32
+#define CHUNK_SIZE 20
 
 // The generateMagicSquare() function is supposed to generate a large matrix square from two smaller ones.
 //
