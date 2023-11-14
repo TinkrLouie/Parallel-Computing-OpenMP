609970229a2e09627adb13b8379065ca813a3693
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-14 04:14:10 +0000

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -34,17 +34,17 @@ void generateMagicSquare(int** pattern, int** modifier, int** magicSquare, int N
     }
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    //#pragma omp parallel for collapse(2)
-    //for (int i = 0; i < M; i++)
-    //{
-    //    for (int j = 0; j < M; j++)
-    //    {
-    //        int patternRow = i % N;
-    //        int patternCol = j % N;
-    //        magicSquare[i][j] = pattern[patternRow][patternCol];
-	//        magicSquare[i][j] += modifier[i/N][j/N];
-    //    }
-    //}
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            int patternRow = i % N;
+            int patternCol = j % N;
+            magicSquare[i][j] = pattern[patternRow][patternCol];
+	        magicSquare[i][j] += modifier[i/N][j/N];
+        }
+    }
 
     //const int blockSize = 32;  // Experiment with different block sizes
     ////----------------------------------------------------------------
@@ -70,23 +70,23 @@ void generateMagicSquare(int** pattern, int** modifier, int** magicSquare, int N
     //    }
     //}
 
-    #pragma omp parallel for shared(pattern, modifier, magicSquare)
-    for (int i = 0; i < M; i++)
-    {
-        int patternRow = i % N;
-        int modifierRow = i / N;
+    //#pragma omp parallel for shared(pattern, modifier, magicSquare)
+    //for (int i = 0; i < M; i++)
+    //{
+    //    int patternRow = i % N;
+    //    int modifierRow = i / N;
 
-        int* patternRowPtr = pattern[patternRow];
-        int* modifierRowPtr = modifier[modifierRow];
+    //    int* patternRowPtr = pattern[patternRow];
+    //    int* modifierRowPtr = modifier[modifierRow];
 
-        for (int j = 0; j < M; j++)
-        {
-            int patternCol = j % N;
-            int modifierCol = j / N;
+    //    for (int j = 0; j < M; j++)
+    //    {
+    //        int patternCol = j % N;
+    //        int modifierCol = j / N;
 
-            magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
-        }
-    }
+    //        magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
+    //    }
+    //}
 }
 
 // computes sum of elements in a row
