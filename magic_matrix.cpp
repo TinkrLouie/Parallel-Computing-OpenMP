35ee37075f43dfde37d25c4592428009233d1aea
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-15 22:41:21 +0000

    Update magic_matrix.cpp

--- magic_matrix.cpp
+++ magic_matrix.cpp
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <omp.h>
 #include <unordered_set>
+#define CHUNK_SIZE 32
 
 // The generateMagicSquare() function is supposed to generate a large matrix square from two smaller ones.
 //
@@ -24,7 +25,7 @@ void generateMagicSquare(int** pattern, int** modifier, int** magicSquare, int N
 {   
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for collapse(2) schedule(static)
+    #pragma omp parallel for collapse(2) schedule(guided)
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
@@ -33,47 +34,91 @@ void generateMagicSquare(int** pattern, int** modifier, int** magicSquare, int N
 	    }
     }
 
-
-    //const int blockSize = 16;  // Experiment with different block sizes
-    ////----------------------------------------------------------------
-    //// OpenMP here!!!-------------------------------------------------
-    //#pragma omp parallel for collapse(2) shared(magicSquare, pattern, modifier)
-    //for (int iOuter = 0; iOuter < M; iOuter += blockSize)
+    // VERSION 1
+    //----------------------------------------------------------------
+    // OpenMP here!!!-------------------------------------------------
+    //#pragma omp parallel for collapse(2)
+    //for (int i = 0; i < M; i++)
     //{
-    //    for (int jOuter = 0; jOuter < M; jOuter += blockSize)
+    //    for (int j = 0; j < M; j++)
     //    {
-    //        for (int i = iOuter; i < iOuter + blockSize && i < M; i++)
-    //        {
-    //            int patternRow = i % N;
-    //            int modifierRow = i / N;
-
-    //            int* patternRowPtr = pattern[patternRow];
-    //            int* modifierRowPtr = modifier[modifierRow];
-
-    //            for (int j = jOuter; j < jOuter + blockSize && j < M; j++)
-    //            {
-    //                int patternCol = j % N;
-    //                int modifierCol = j / N;
-
-    //                magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
-    //            }
-    //        }
+    //        int patternRow = i % N;
+    //        int patternCol = j % N;
+    //        magicSquare[i][j] = pattern[patternRow][patternCol];
+	//        magicSquare[i][j] += modifier[i/N][j/N];
     //    }
     //}
 
+    // VERSION 2
+    //#pragma omp parallel for shared(pattern, modifier, magicSquare)
+    //for (int i = 0; i < M; i++)
+    //{
+    //    int patternRow = i % N;
+    //    int modifierRow = i / N;
+
+    //    int* patternRowPtr = pattern[patternRow];
+    //    int* modifierRowPtr = modifier[modifierRow];
+
+    //    for (int j = 0; j < M; j++)
+    //    {
+    //        int patternCol = j % N;
+    //        int modifierCol = j / N;
+
+    //        magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
+    //    }
+    //}
+
+    // VERSION 3
+    int iOuter, jOuter;
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for collapse(2) schedule(static)
-    for (int i = 0; i < M; i++)
+    #pragma omp parallel for collapse(2) shared(magicSquare, pattern, modifier) private(iOuter, jOuter) schedule(guided)
+    for (iOuter = 0; iOuter < M; iOuter += CHUNK_SIZE)
     {
-        for (int j = 0; j < M; j++)
-        {
-            int patternRow = i % N;
-            int patternCol = j % N;
-            magicSquare[i][j] = pattern[patternRow][patternCol];
-	        magicSquare[i][j] += modifier[i/N][j/N];
+        for (jOuter = 0; jOuter < M; jOuter += CHUNK_SIZE)
+        {   
+            for (int i = iOuter; i < iOuter + CHUNK_SIZE && i < M; i++)
+            {   
+                int patternRow = i % N;
+                int modifierRow = i / N;
+                int* patternRowPtr = pattern[patternRow];
+                int* modifierRowPtr = modifier[modifierRow];
+                for (int j = jOuter; j < jOuter + CHUNK_SIZE && j < M; j++)
+                {
+                    int patternCol = j % N;
+                    int modifierCol = j / N;
+                    magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
+                }
+            }
         }
     }
+
+    // VERSION 4
+    //int body_start_index;
+    //////----------------------------------------------------------------
+    ////// OpenMP here!!!-------------------------------------------------
+    //#pragma omp parallel private(body_start_index) shared(magicSquare, pattern, modifier)
+    //for(body_start_index = 0; body_start_index < M - CHUNK_SIZE; body_start_index += CHUNK_SIZE) {
+    //    int i;
+    //    int body_end_index = body_start_index + CHUNK_SIZE;
+    //    ////----------------------------------------------------------------
+    //    //// OpenMP here!!!-------------------------------------------------
+    //    #pragma omp for private(i) schedule(guided)
+    //    #pragma unroll_and_jam (4)
+    //    for(i = body_start_index; i < body_end_index; i++) {
+    //        int j;
+    //        int patternRow = i % N;
+    //        int modifierRow = i / N;
+    //        int* patternRowPtr = pattern[patternRow];
+    //        int* modifierRowPtr = modifier[modifierRow];            
+    //        for(j = body_start_index; j < body_end_index; j++) {
+    //            int patternCol = j % N;
+    //            int modifierCol = j / N;
+    //            magicSquare[i][j] = patternRowPtr[patternCol] + modifierRowPtr[modifierCol];
+    //        }
+    //
+    //    }
+    //}
 }
 
 // computes sum of elements in a row
@@ -82,7 +127,7 @@ int sumRow( int** matrix, int row, int N)
     int sum = 0;
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for reduction(+:sum) schedule(static)
+    #pragma omp parallel for reduction(+:sum) schedule(guided)
     for (int i = 0; i < N; i++)
     {
         sum += matrix[row][i];
@@ -96,7 +141,7 @@ int sumColumn( int** matrix, int col, int N)
     int sum = 0;
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for reduction(+:sum) schedule(static)
+    #pragma omp parallel for reduction(+:sum) schedule(guided)
     for (int i = 0; i < N; i++)
     {
         sum += matrix[i][col];
@@ -105,24 +150,46 @@ int sumColumn( int** matrix, int col, int N)
 }
 
 // checks if all elements in an array are equal
-// No need for parallel
 bool allEqual( int arr[], int N)
 {   
+    double aEs, aEe;
+    aEs = omp_get_wtime();
     for (int i = 0; i < N; i++){
         if (arr[0] != arr[i])
-	    {
+	{
             return false;
         }
     }
+    aEe = omp_get_wtime();
+    printf("allEqual computation time: %.15f\n", aEe - aEs);
     return true;
 }
 
+//bool allEqual(int arr[], int N) {
+//    bool result = true;
+//
+//    #pragma omp parallel for shared(result)
+//    for (int i = 1; i < N; i++) {
+//        // Use a critical section to update the result variable
+//        #pragma omp critical
+//        {
+//            if (arr[0] != arr[i]) {
+//                result = false;
+//            }
+//        }
+//    }
+//
+//    return result;
+//}
+
 bool isPairwiseDistinct( int** matrix, int N) {
+    double iPDs, iPDe;
+    iPDs = omp_get_wtime();
     bool found = false;
     std::unordered_set<int> elementSet;
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for collapse(2) schedule(static) shared(found, elementSet)
+    #pragma omp parallel for collapse(2) shared(found, elementSet) schedule(guided)
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             int currentElement = matrix[i][j];
@@ -134,14 +201,12 @@ bool isPairwiseDistinct( int** matrix, int N) {
                     elementSet.insert(currentElement);
                 }
             }
-            //if (found) {
-            //    //To break out of the loop
-            //    #pragma omp cancel for
-            //}
         }
     }
+    iPDe = omp_get_wtime();
+    printf("isPairwiseDistinct computation time: %.15f\n", iPDe - iPDs);
     return found;
-}   
+}  
 
 // checks if matrix is a magic square
 bool isMagicSquare(int** matrix, int N)
@@ -151,31 +216,10 @@ bool isMagicSquare(int** matrix, int N)
     int main_diag_sum = 0;
     int anti_diag_sum = 0;
 
-    //----------------------------------------------------------------
-    // OpenMP here!!!-------------------------------------------------
-    //#pragma omp parallel for //reduction(+:main_diag_sum, anti_diag_sum) schedule(static)
-    //for (int i = 0; i < N; i++)
-    //{   
-    //    // compute row sums
-    //    row_sums[i] = sumRow(matrix, i, N);
-    //    // compute column sums
-    //    col_sums[i] = sumColumn(matrix, i, N);
-    //     // compute sum of elements on main diagonal
-    //    main_diag_sum += matrix[i][i];
-    //    // compute sum of elements on antidiagonal
-    //    anti_diag_sum += matrix[i][N - 1 - i];
-    //}
-    //int row_sum = row_sums[0];  
-    //if (!allEqual(row_sums, N)) return false;
-    //if (!allEqual(col_sums, N)) return false;
-    //if (main_diag_sum != row_sum) return false;
-    //if (anti_diag_sum != row_sum) return false;
-
-
     // compute row sums
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(guided)
     for (int i = 0; i < N; i++)
     {
         row_sums[i] = sumRow(matrix, i, N);
@@ -187,7 +231,7 @@ bool isMagicSquare(int** matrix, int N)
     // compute column sums
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(guided)
     for (int i = 0; i < N; i++)
     {
         col_sums[i] = sumColumn(matrix, i, N);
@@ -197,7 +241,7 @@ bool isMagicSquare(int** matrix, int N)
     // compute sum of elements on main diagonal
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for reduction(+:main_diag_sum) schedule(static)
+    #pragma omp parallel for reduction(+:main_diag_sum) schedule(guided)
     for (int i = 0; i < N; i++)
     {
         main_diag_sum += matrix[i][i];
@@ -207,36 +251,35 @@ bool isMagicSquare(int** matrix, int N)
     // compute sum of elements on antidiagonal
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for reduction(+:anti_diag_sum) schedule(static)
+    #pragma omp parallel for reduction(+:anti_diag_sum) schedule(guided)
     for (int i = 0; i < N; i++)
     {
         anti_diag_sum += matrix[i][N - 1 - i];
     }
     if (anti_diag_sum != row_sum) return false;
     
-    if(isPairwiseDistinct(matrix, N))
-	    return false;
-    return true;
+    return !isPairwiseDistinct(matrix, N);
 }
 
 int main(int argc, char *argv[])
 {
-
-    // Timer Init
-    double itime, ftime, exec_time;
 
     if (argc != 3) {
         printf("Usage: %s <pattern_filename> <modifier_filename>\n", argv[0]);
         return 1;
     }
 
-    // Code for checking teams and threads num
-    // int num_teams= omp_get_num_teams(); 
-    // int num_threads_per_team = omp_get_num_threads();
-    // printf("Running on GPU with %d teams and %d threads per team\n", 
-    //   num_teams, 
-    //   num_threads_per_team
-    // );
+    // Timer Init
+    double itime, ftime, exec_time, gMSe, gMSt, iMSt, start, end;
+
+    start = omp_get_wtime();
+
+    //int num_teams= omp_get_num_teams(); 
+    //int num_threads_per_team = omp_get_num_threads();
+    //printf("Running on GPU with %d teams and %d threads per team\n", 
+    //  num_teams, 
+    //  num_threads_per_team
+    //);
 
     FILE *pattern_file = fopen(argv[1], "r");
     FILE *modifier_file = fopen(argv[2], "r");
@@ -271,20 +314,14 @@ int main(int argc, char *argv[])
     int M = N*N;
 
     int** magicSquare = new int*[M];
-    
-    // CAN PARALLEL
     //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for
     for (int i = 0; i < M; i++) {
 	    magicSquare[i] = new int[M];
     }
 
     // read-in matrix data
-    //int ret = 0;
-    //----------------------------------------------------------------
-    // OpenMP here!!!-------------------------------------------------
-    //#pragma omp parallel for schedule(dynamic, d)
     for (int i = 0; i < N; i++) {
 	    pattern[i] = new int[N];
 	    modifier[i] = new int[N];
@@ -293,25 +330,14 @@ int main(int argc, char *argv[])
                 printf("Error reading matrix values pattern.\n");
                 fclose(pattern_file);
                 return 1;
-                //ret = 1;
             }
             if (fscanf(modifier_file, "%d", &modifier[i][j]) != 1) {
                 printf("Error reading matrix values modifier.\n");
                 fclose(modifier_file);
                 return 1;
-                //ret = 2;
             }
         }
     }
-
-    //if (ret == 1) {
-    //    printf("Error reading matrix values pattern.\n");
-    //    return 1;
-    //}
-    //if (ret == 2) {
-    //    printf("Error reading matrix values modifier.\n");
-    //    return 1;
-    //}
 
     fclose(pattern_file);
     fclose(modifier_file);
@@ -324,9 +350,10 @@ int main(int argc, char *argv[])
     itime = omp_get_wtime();
 
     generateMagicSquare(pattern, modifier, magicSquare, N, M);
+    gMSe = omp_get_wtime();
 
     bool is_magic_square = isMagicSquare(magicSquare, M);
-    
+
     //-------------------------------------//
     //BOOL FOR DETERMINING MAGIC SQUARE----//
     //-------------------------------------//
@@ -334,45 +361,50 @@ int main(int argc, char *argv[])
     // Timer end
     ftime = omp_get_wtime();
 
+    // Timer print out
+    exec_time = ftime - itime;
+    gMSt = gMSe - itime;
+    iMSt = ftime - gMSe;
+    
+    printf("generateMagicSquare computation time: %.15f\n", gMSt);
+    printf("isMagicSquare computation time: %.15f\n", iMSt);
+    printf("Total computation time: %.15f\n", exec_time);
+    printf("\n");
+
     // Print first 3 and last 3 elements of generated and checked matrix 
     for (int i = 0; i < 3; i++)
     {
         printf("%d ", magicSquare[i][0]);
     }
-
     printf("... \n . \n . \n . \n");
-
     for (int j = M-3; j < M; j++)
     {
         printf("%d ", magicSquare[M-1][j]);
     }
-
     printf("\n");
     if(is_magic_square) printf("Generated matrix is a magic square.\n");
     else                printf("Generated matrix is not a magic square.\n");
 
-    // Timer print out
-    exec_time = ftime - itime;
-    printf("\n");
-    printf("Total computation time: %.15f\n", exec_time);
-
     // CAN TRY TO PARALLEL
     // free dynamically allocated memory
-     //----------------------------------------------------------------
+    //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for
     for (int i = 0; i < M; i++) {
         delete[] magicSquare[i];
     }
     delete[] magicSquare;
 
-     //----------------------------------------------------------------
+    //----------------------------------------------------------------
     // OpenMP here!!!-------------------------------------------------
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for
     for (int i = 0; i < N; i++) {
 	    delete[] pattern[i];
 	    delete[] modifier[i];
     }
     delete[] pattern;
     delete[] modifier;
+    
+    end = omp_get_wtime();
+    printf("Total runtime: %.15f\n", end - start);
 }
