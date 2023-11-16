55e7da57bfcce2b579bb80a18c8721c9f8119d62
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-16 04:16:29 +0000

    Update install_numact1.sh

--- install_numact1.sh
+++ install_numact1.sh
@@ -2,39 +2,43 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
-sudo -n true
-test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
-
 # Set the version of numactl to install
 NUMACTL_VERSION="2.0.16"
 
 # Set the installation directory in your home directory
 INSTALL_DIR="home2/nlzl16"
 
-# Create a temporary directory for the build
-TEMP_DIR=$(mktemp -d)
+read -p "This script will install numactl version $NUMACTL_VERSION in $INSTALL_DIR. Continue? (y/n): " choice
 
-# Navigate to the temporary directory
-cd "$TEMP_DIR" || exit
+# Check the user's choice
+if [[ $choice =~ ^[Yy]$ ]]; then
+    # Create a temporary directory for the build
+    TEMP_DIR=$(mktemp -d)
 
-# Download the numactl source tarball
-wget https://github.com/numactl/numactl/releases/download/v${NUMACTL_VERSION}/numactl-${NUMACTL_VERSION}.tar.gz
+    # Navigate to the temporary directory
+    cd "$TEMP_DIR" || exit
 
-# Extract the tarball
-tar -xf numactl-${NUMACTL_VERSION}.tar.gz
+    # Download the numactl source tarball
+    wget https://github.com/numactl/numactl/releases/download/v${NUMACTL_VERSION}/numactl-${NUMACTL_VERSION}.tar.gz
 
-# Navigate into the numactl source directory
-cd numactl-${NUMACTL_VERSION} || exit
+    # Extract the tarball
+    tar -xf numactl-${NUMACTL_VERSION}.tar.gz
 
-# Configure the build to install in the specified directory
-./configure --prefix="$INSTALL_DIR"
+    # Navigate into the numactl source directory
+    cd numactl-${NUMACTL_VERSION} || exit
 
-# Build and install numactl
-make && make install
+    # Configure the build to install in the specified directory
+    ./configure --prefix="$INSTALL_DIR"
 
-# Clean up temporary files
-cd "home2/nlzl16" || exit
-rm -rf "$TEMP_DIR"
+    # Build and install numactl
+    make && make install
 
-echo "Numactl version ${NUMACTL_VERSION} has been installed in $INSTALL_DIR"
-echo "Make sure to add $INSTALL_DIR/bin to your PATH environment variable."
+    # Clean up temporary files
+    cd "home2/nlzl16" || exit
+    rm -rf "$TEMP_DIR"
+
+    echo "Numactl version ${NUMACTL_VERSION} has been installed in $INSTALL_DIR"
+    echo "Make sure to add $INSTALL_DIR/bin to your PATH environment variable."
+else
+    echo "Installation failed."
+fi
