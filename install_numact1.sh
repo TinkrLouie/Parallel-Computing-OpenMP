50f78763771933be5de4e4119fc5fdf5f8d73501
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-16 04:36:24 +0000

    Update install_numact1.sh

--- install_numact1.sh
+++ install_numact1.sh
@@ -1,46 +1,36 @@
 #!/bin/bash
 
-set -eu -o pipefail # fail on error and report it, debug all lines
+set -eu -o pipefail
 
-# Set the version of numactl to install
 NUMACTL_VERSION="2.0.16"
 
 TARGET="numact1"
 
-# Set the installation directory in your home directory
 INSTALL_DIR="$HOME/$TARGET"
 
 read -p "This script will install numactl version $NUMACTL_VERSION in $INSTALL_DIR. Continue? (y/n): " choice
 
-# Check the user's choice
 if [[ $choice =~ ^[Yy]$ ]]; then
-    # Create a temporary directory for the build
     TEMP_DIR=$(mktemp -d)
 
-    # Navigate to the temporary directory
     cd "$TEMP_DIR" || exit
 
-    # Download the numactl source tarball
     wget https://github.com/numactl/numactl/releases/download/v${NUMACTL_VERSION}/numactl-${NUMACTL_VERSION}.tar.gz
 
-    # Extract the tarball
     tar -xf numactl-${NUMACTL_VERSION}.tar.gz
 
-    # Navigate into the numactl source directory
     cd numactl-${NUMACTL_VERSION} || exit
 
-    # Configure the build to install in the specified directory
     ./configure --prefix="$INSTALL_DIR"
 
-    # Build and install numactl
     make && make install
 
-    # Clean up temporary files
     cd "$HOME" || exit
     rm -rf "$TEMP_DIR"
-
+    echo -e
+    echo "---------------------------------------------------------------------"
     echo "Numactl version ${NUMACTL_VERSION} has been installed in $INSTALL_DIR"
-    echo "Make sure to add $INSTALL_DIR/bin to your PATH environment variable."
+    echo "---------------------------------------------------------------------"
 else
     echo "Installation failed."
 fi
