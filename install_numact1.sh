9d66efe103757008945649ccd7ec1134084964c7
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2023-11-17 21:16:20 +0000

    Update install_numact1.sh

--- install_numact1.sh
+++ install_numact1.sh
@@ -7,6 +7,7 @@ NUMACTL_VERSION="2.0.16"
 TARGET="numact1"
 
 INSTALL_DIR="$HOME/$TARGET"
+INSTALL_PATH="$PATH"
 
 read -p "This script will install numactl version $NUMACTL_VERSION in $INSTALL_DIR. Continue? (y/n): " choice
 
@@ -22,15 +23,17 @@ if [[ $choice =~ ^[Yy]$ ]]; then
     cd numactl-${NUMACTL_VERSION} || exit
 
     ./configure --prefix="$INSTALL_DIR"
+    make && make install
 
+    ./configure --prefix="$INSTALL_PATH"
     make && make install
 
     cd "$HOME" || exit
     rm -rf "$TEMP_DIR"
     echo -e
-    echo "---------------------------------------------------------------------"
-    echo "Numactl version ${NUMACTL_VERSION} has been installed in $INSTALL_DIR"
-    echo "---------------------------------------------------------------------"
+    echo "------------------------------------------------------------------------------"
+    echo "Numactl version ${NUMACTL_VERSION} has been installed in $INSTALL_DIR and PATH"
+    echo "------------------------------------------------------------------------------"
 else
     echo "Installation failed."
 fi
